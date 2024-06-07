5c5fb0ff370391b2376cf9fdd0d4eb4f36af3b73
batuhan <107402252+batuhankanra@users.noreply.github.com>
2024-06-07 22:09:38 +0300

    complate login register logout

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express =require('express')
 const postRoute = require('./routes/post.route')
 const authRoute = require('./routes/auth.route')
+const cookieParser = require('cookie-parser')
 
 
 const app=express()
+
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/posts",postRoute)
 app.use("/api/auth",authRoute)
 
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const bcrypt=require('bcrypt')
 const prisma = require('../lib/prisma.js')
+const jwt=require('jsonwebtoken')
 
 const register=async (req,res)=>{
     const {username,email,password}=req.body
@@ -22,11 +23,47 @@ const register=async (req,res)=>{
         res.status(500).json({message:"Failed to Created User"})
     }
 }
-const login= (req,res)=>{
-    //db operations
+const login= async(req,res)=>{
+    const {username,password}=req.body;
+
+    try{
+        //Check if the user exits
+        const user = await prisma.user.findUnique({
+            where:{username}
+        })
+        if(!user){
+            return res.status(404).json({message:"user not found"})
+        }
+
+        //Check if the password is correct
+        const isPasswordValid = await bcrypt.compare(password,user.password)
+        if(!isPasswordValid){
+            return res.status(404).json({message:"password not found"})
+        }
+
+
+        //Generate cookie token and send to the user
+        //res.setHeader("Set-Cookie","test="+"myValue").json({message:"success"})
+        const age=1000*60*60*24*7
+        const token =jwt.sign({
+            id:user.id
+        },process.env.JWT_SECRET_KEY,
+        {expiresIn:age}
+        )
+        res.cookie('test2',token,{
+            httpOnly:true,
+            //secure:true,
+            maxAge:age
+        }).status(200).json({message:"login successful"})
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message:"failed to login!"})
+    }
+
 }
 const logout= (req,res)=>{
-    //db operations
+    res.clearCookie("token").status(200).json({message:"logout successful"})
 }
 
 module.exports={register,login,logout}
